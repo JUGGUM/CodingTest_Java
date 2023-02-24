b434fec0767f5927532af333bf572ad98adec243
GAYOUNG <107148237+JUGGUM@users.noreply.github.com>
2023-02-24 09:25:25 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int angle) {
+        int answer = 0;
+        if(0<angle && angle <90){
+            answer = 1;
+            return answer;
+        }else if(angle == 90){
+            answer = 2;
+            return answer;
+        }else if(90< angle && angle<180){
+            answer = 3;
+            return answer;
+        }else if(angle ==180){
+            answer = 4;
+            return answer;
+        }
+        return answer;
+    }
+}
